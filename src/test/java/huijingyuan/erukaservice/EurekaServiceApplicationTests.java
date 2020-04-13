2b0af31e2520e57168bd64554af4c773cab3dad6
cheeser <649457551@qq.com>
2020-04-14 01:11:35 +0800

    添加Eureka的服务提供者

---
+++ src/test/java/huijingyuan/erukaservice/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package huijingyuan.erukaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
